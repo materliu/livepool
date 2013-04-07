1bf53cbea3e026dcb54eb4bf6706bc34a0533230
rehorn <sysucgh@163.com>
2013-04-07 18:33:52 +0800

    updated

--- rules/project2.js
+++ rules/project2.js
@@ -2,8 +2,24 @@ module.exports = {
     name: "project2",
     index: 0,
     enabled: 1,
-    base: "E:/connect/bapp/widget_trunk/web/chat/src",
+    base: "E:/connect/bapp/widget_trunk/web/chat/src/",
     handler: [{
+        match: "chat.qq.com/index.html",
+        action: "./__index.html",
+        enabled: 1
+    }, {
+        match: "chat.qq.com/js/chat.all.js",
+        action: "./tools/chat.all.qzmin",
+        enabled: 1
+    }, {
+        match: "chat.qq.com/css/main.css",
+        action: ["./css/sub1.css", "./css/sub2.css"],
+        enabled: 1
+    }, {
+        match: "chat.qq.com/css/remote.css",
+        action: "http://chat.qq.com/css/remote2.css",
+        enabled: 1
+    }, {
         match: "chat.qq.com/ *.url.cn/chat/",
         action: "./",
         enabled: 1
