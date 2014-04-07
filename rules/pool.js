2b161d2cea3a0c366ca1a3903d15850caeebca15
rehorn <sysucgh@gmail.com>
2014-04-08 00:20:48 +0800

    bump 0.5

--- rules/pool.js
+++ rules/pool.js
@@ -1,10 +1,13 @@
 module.exports = {
-    name: 'livepool',
+    name: "livepool",
+    type: "root",
     children: [{
         name: "project1",
+        type: "proj",
         match: "/Users/rehorn/Documents/Code/node/livepool/test/examples/",
         children: [{
-            name: 'handler',
+            name: "handler",
+            type: "group",
             children: [{
                 match: "find.qq.com/index.html",
                 action: "./__index.html"
@@ -25,7 +28,62 @@ module.exports = {
                 action: "./"
             }]
         }, {
+            type: "group",
+            name: 'mocker',
+            children: [{
+                match: "find.qq.com/cgi-bin/find_v10",
+                action: "{code:0, data}"
+            }]
+        }, {
             name: 'router',
+            type: "group",
+            children: [{
+                match: "find.qq.com/cgi-bin/|cgi.find.qq.com",
+                action: "-"
+            }, {
+                match: "find.qq.com|idqqimg.com/qqfind/",
+                action: "172.23.136.84"
+            }]
+        }]
+    }, {
+        name: "project2",
+        type: "proj",
+        match: "/Users/rehorn/Documents/Code/node/livepool/test/examples/",
+        children: [{
+            name: "handler",
+            type: "group",
+            children: [{
+                match: "find.qq.com/index.html",
+                action: "./__index.html"
+            }, {
+                match: "find.qq.com/js/find.all.js",
+                action: "./tools/find.all.qzmin"
+            }, {
+                match: "find.qq.com/js/find.combo.js",
+                action: "./js/jquery.js|./js/main.js"
+            }, {
+                match: "find.qq.com/css/comp.css",
+                action: "./css/main.css|./css/sub1.css"
+            }, {
+                match: "find.qq.com/js/catalog.js",
+                action: "http://pub.idqqimg.com/qqfind/js/groupcatalog.js"
+            }, {
+                match: "find.qq.com/|*.idqqimg.com/qqfind/",
+                action: "./"
+            }]
+        }, {
+            type: "group",
+            name: 'mocker',
+            children: [{
+                match: "find.qq.com/cgi-bin/|cgi.find.qq.com",
+                action: "-"
+            }, {
+                match: "find.qq.com|idqqimg.com/qqfind/",
+                action: "172.23.136.84"
+            }]
+        }, {
+            name: 'router',
+            type: "group",
             children: [{
                 match: "find.qq.com/cgi-bin/|cgi.find.qq.com",
                 action: "-"
--- test.js
+++ test.js
@@ -1,55 +1,57 @@
-records: [{
-    "name": "",
-    "sort": 0,
-    "match": "find.qq.com/index.html",
-    "action": "./__index.html",
+{
+    "name": "project1",
+    "type": "proj",
+    "match": "/Users/rehorn/Documents/Code/node/livepool/test/examples/",
+    "id": 1,
     "checked": true,
-    "id": null,
-    "parentId": null,
-    "leaf": true
-}, {
-    "name": "",
-    "sort": 4,
-    "match": "find.qq.com/css/comp.css",
-    "action": "./css/main.css|./css/sub1.css",
-    "checked": true,
-    "id": null,
-    "parentId": null,
-    "leaf": true
-}, {
-    "name": "",
-    "sort": 5,
-    "match": "find.qq.com/js/catalog.js",
-    "action": "http://pub.idqqimg.com/qqfind/js/groupcatalog.js",
-    "checked": true,
-    "id": null,
-    "parentId": null,
-    "leaf": true
-}, {
-    "name": "",
-    "sort": 3,
-    "match": "find.qq.com/js/find.combo.js",
-    "action": "./js/jquery.js|./js/main.js",
-    "checked": true,
-    "id": null,
-    "parentId": null,
-    "leaf": true
-}, {
-    "name": "",
-    "sort": 1,
-    "match": "find.qq.com/|*.idqqimg.com/qqfind/",
-    "action": "./",
-    "checked": true,
-    "id": null,
-    "parentId": null,
-    "leaf": true
-}, {
-    "name": "",
-    "sort": 2,
-    "match": "find.qq.com/js/find.all.js",
-    "action": "./tools/find.all.qzmin",
-    "checked": true,
-    "id": null,
-    "parentId": null,
-    "leaf": true
-}]
+    "handler": [{
+        "match": "find.qq.com/index.html",
+        "action": "./__index.html",
+        "leaf": true,
+        "id": 3,
+        "checked": true
+    }, {
+        "match": "find.qq.com/js/find.all.js",
+        "action": "./tools/find.all.qzmin",
+        "leaf": true,
+        "id": 4,
+        "checked": true
+    }, {
+        "match": "find.qq.com/js/find.combo.js",
+        "action": "./js/jquery.js|./js/main.js",
+        "leaf": true,
+        "id": 5,
+        "checked": true
+    }, {
+        "match": "find.qq.com/css/comp.css",
+        "action": "./css/main.css|./css/sub1.css",
+        "leaf": true,
+        "id": 6,
+        "checked": true
+    }, {
+        "match": "find.qq.com/js/catalog.js",
+        "action": "http://pub.idqqimg.com/qqfind/js/groupcatalog.js",
+        "leaf": true,
+        "id": 7,
+        "checked": true
+    }, {
+        "match": "find.qq.com/|*.idqqimg.com/qqfind/",
+        "action": "./",
+        "leaf": true,
+        "id": 8,
+        "checked": true
+    }],
+    "router": [{
+        "match": "find.qq.com/cgi-bin/|cgi.find.qq.com",
+        "action": "-",
+        "leaf": true,
+        "id": 13,
+        "checked": true
+    }, {
+        "match": "find.qq.com|idqqimg.com/qqfind/",
+        "action": "172.23.136.84",
+        "leaf": true,
+        "id": 14,
+        "checked": true
+    }]
+}
