c29bc25614e3176d14a81ff672300dd9c524abdc
rehorn <sysucgh@gmail.com>
2014-05-01 20:47:56 +0800

    bump 0.3.0 lots of update

---
+++ rules/settings.js
@@ -0,0 +1,12 @@
+module.exports={
+    "keep": false,
+    "proxy": false,
+    "disablePool": false,
+    "disableCache": false,
+    "autoScroll": false,
+    "netSimulate": "10m",
+    "showSession": "200",
+    "layout": "Wide",
+    "tabSetting": "Pool,Inspectors,Composer,Filter,Log",
+    "sessionColumns": "Protocol,Method,Host,Path,Caching,ContentType,Body,Time"
+}
